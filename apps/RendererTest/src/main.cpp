4a006e17aa77e496ef42b0538497708011a9b9d9
Meliothep <meliothep.kun@gmail.com>
2025-09-15 04:44:44 +0200

    DeviceManager and Win32Window

--- apps/RendererTest/src/main.cpp
+++ apps/RendererTest/src/main.cpp
@@ -1,8 +1,19 @@
+#include <Utils/Logger.hpp>
+#include <Utils/Win32ConsoleStrategy.hpp>
+#include <Windows/Win32Window.hpp>
+
 #include <VulkanRenderer/VulkanRenderer.hpp>
 
 int main() {
-    VulkanRenderer renderer;
-    renderer.hello();
+    auto logger = std::make_unique<Logger>();
+
+    logger->AddLoggerStrat(std::make_unique<Win32ConsoleStrategy>(LogLevel::DEBUG));
+
+    IWindow* window = new Win32Window(500, 500, "TestWindow"); 
+    //IWindow* window = nullptr;    
+
+    VulkanRenderer renderer(*logger);
+    renderer.Initialize(window);
+
     return 0;
-    
 }
