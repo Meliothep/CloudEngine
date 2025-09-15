769c48d9eb9ba53e72fe9b7760c0e5ca3b709073
Meliothep <meliothep.kun@gmail.com>
2025-09-15 04:45:10 +0200

    DeviceManager and Win32Window

---
+++ libs/VulkanRenderer/include/Utils/Logger.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <format>
+#include <mutex>
+
+enum class LogLevel { DEBUG = 0, INFO = 1, EXCEPT = 2, CRITICAL = 3 };
+
+class LoggerStrategy
+{
+public:
+    explicit LoggerStrategy(LogLevel logLevel) : logLevel_(logLevel) {}
+    virtual ~LoggerStrategy() = default;
+    virtual void Log(LogLevel logLevel, const std::string& message) = 0;
+
+protected:
+    LogLevel logLevel_;
+    std::mutex mutex_;
+};
+
+class Logger
+{
+public:
+    Logger() = default;
+    ~Logger() = default;
+
+    void AddLoggerStrat(std::unique_ptr<LoggerStrategy> strat) {
+        mp_strategies.push_back(std::move(strat));
+    }
+
+    void RemoveLoggerStrat(LoggerStrategy* strat) {
+        auto it = std::remove_if(mp_strategies.begin(), mp_strategies.end(),
+            [strat](const std::unique_ptr<LoggerStrategy>& ptr) { return ptr.get() == strat; });
+        mp_strategies.erase(it, mp_strategies.end());
+    }
+
+    void Log(LogLevel logLevel, const std::string& message) {
+        for (auto& strat : mp_strategies) {
+            strat->Log(logLevel, message);
+        }
+    }
+
+    template <typename... T>
+    void Log(LogLevel logLevel, std::string message, T&&... args) {
+        try {
+            Log(logLevel, std::format(message, std::forward<T>(args)...));
+        }
+        catch (const std::format_error& e) {
+            Log(LogLevel::EXCEPT, "Formatting error: " + std::string(e.what()));
+        }
+    }
+
+private:
+    std::vector<std::unique_ptr<LoggerStrategy>> mp_strategies;
+};
+
---
+++ libs/VulkanRenderer/include/Utils/Win32ConsoleStrategy.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "Logger.hpp"
+
+#include <Windows.h>
+#include <ctime>
+#include <iostream>
+
+class Win32ConsoleStrategy : public LoggerStrategy
+{
+public:
+    explicit Win32ConsoleStrategy(LogLevel logLevel) : LoggerStrategy(logLevel) {}
+    
+    void Log(LogLevel logLevel, const std::string& message) override {
+        if (logLevel < logLevel_) return;
+
+        std::lock_guard<std::mutex> lock(mutex_);
+
+        time_t now = time(0);
+        tm timeinfo{};
+        localtime_s(&timeinfo, &now);
+        char timestamp[20];
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &timeinfo);
+
+        std::cout << "[" << timestamp << "] ";
+
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        switch (logLevel) {
+        case LogLevel::DEBUG:    SetConsoleTextAttribute(hConsole, 0x02); std::cout << "DEBUG "; break;
+        case LogLevel::INFO:     SetConsoleTextAttribute(hConsole, 0x01); std::cout << "INFO "; break;
+        case LogLevel::EXCEPT:   SetConsoleTextAttribute(hConsole, 0x06); std::cout << "EXCEPT "; break;
+        case LogLevel::CRITICAL: SetConsoleTextAttribute(hConsole, 0x0C); std::cout << "CRITICAL "; break;
+        }
+        SetConsoleTextAttribute(hConsole, 0x0F);
+        std::cout << ": " << message << std::endl;
+    }
+};
