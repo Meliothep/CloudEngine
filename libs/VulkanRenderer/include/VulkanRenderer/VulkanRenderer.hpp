0fe5ed4cdb35095b6d4f7f41d441891cd7289feb
Meliothep <meliothep.kun@gmail.com>
2025-09-18 00:30:42 +0200

    feat: Better window handling

--- libs/VulkanRenderer/include/VulkanRenderer/VulkanRenderer.hpp
+++ libs/VulkanRenderer/include/VulkanRenderer/VulkanRenderer.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include "Utils/Logger.hpp"
 #include "Windows/IWindow.hpp"
+#include "InstanceManager.hpp"
+#include "DeviceManager.hpp"
+#include "SurfaceManager.hpp"
 
 class VulkanRenderer{
 public:
     VulkanRenderer(Logger& logger): logger_(logger){}
     
     void Initialize(IWindow* window = nullptr);
+    void Shutdown();
 private:
     Logger& logger_;
+
+    std::unique_ptr<InstanceManager> instanceManager_ = nullptr;
+    std::unique_ptr<SurfaceManager> surfaceManager_ = nullptr;
+    std::unique_ptr<DeviceManager> deviceManager_ = nullptr;
 };
